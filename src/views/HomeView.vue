0e1c2410b2fe1308c39234ae6dead47fcc89369a
geostojanov <gestojanov@gmail.com>
2022-08-26 07:12:22 +0200

    Lesson 47 Done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>H1</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim quod culpa aliquam soluta ea quam quisquam inventore, voluptas reprehenderit sit, facilis earum impedit sequi, quasi tempore saepe molestiae repellendus eum ducimus dignissimos tenetur dolor. Ab magnam qui temporibus beatae! Error est possimus nihil? Eos error dolorem eum rerum provident nam quidem laudantium corporis. Assumenda blanditiis deserunt vel rem quos odio architecto perspiciatis. Cupiditate tenetur explicabo id fuga magnam porro sint unde repellendus necessitatibus. Molestiae, nisi.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim quod culpa aliquam soluta ea quam quisquam inventore, voluptas reprehenderit sit, facilis earum impedit sequi, quasi tempore saepe molestiae repellendus eum ducimus dignissimos tenetur dolor. Ab magnam qui temporibus beatae! Error est possimus nihil? Eos error dolorem eum rerum provident nam quidem laudantium corporis. Assumenda blanditiis deserunt vel rem quos odio architecto perspiciatis. Cupiditate tenetur explicabo id fuga magnam porro sint unde repellendus necessitatibus. Molestiae, nisi.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {  }
 }
 </script>
